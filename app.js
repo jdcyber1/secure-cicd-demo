b2fc85cb6dcf309a2c44e725d65496e6bf98232d
jdcyber1 <jdcyber1@gmail.com>
2025-12-01 20:03:06 +0530

    initial commit - secure CI/CD pipeline ready

---
+++ app.js
@@ -0,0 +1,9 @@
+﻿const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Secure CI/CD Pipeline Working Perfectly da Machi!</h1><p>Secured by SonarQube + OWASP Dependency-Check</p>');
+});
+
+app.listen(port, () => console.log('App running on port ' + port));
